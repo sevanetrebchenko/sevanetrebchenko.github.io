0e383ebb8017f20adea228f7aa6116c7cdba933c
seva <seva.netrebchenko@gmail.com>
2021-10-11 04:55:13 -0700

    Set up references for footer buttons.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,42 +1,41 @@
-import React, { useRef, useEffect } from "react";
-
+import React from "react";
 import "./footer.css";
 
 export default function Footer(props = {}) {
-  const clickable = useRef();
-  useEffect(() => {
-    // add when mounted
-    document.addEventListener("mousedown", handleClick);
-
-    // return function to be called when unmounted
-    return () => {
-      document.removeEventListener("mousedown", handleClick);
-    };
-  }, []);
-
-  const handleClick = (e) => {
-    if (!clickable.current.contains(e.target)) {
-    }
-  };
-
   return (
     <React.Fragment>
       <div className="footer">
 
         <button className="footer-button">
-          <i className="fas fa-envelope footer-icon" ref={clickable}/>
+          <i className="fas fa-envelope footer-icon" 
+             onClick = {(e) => { 
+              e.preventDefault();
+              window.location = 'mailto:seva.netrebchenko@gmail.com';
+            }}/>
         </button>
 
         <button className="footer-button">
-          <i className="fab fa-github footer-brand-icon" ref={clickable}/>
+          <i className="fab fa-github footer-brand-icon"
+              onClick = {(e) => { 
+              e.preventDefault();
+              window.location.href = 'https://github.com/sevanetrebchenko/';
+            }}/>
         </button>
 
         <button className="footer-button">
-          <i className="fab fa-linkedin footer-brand-icon" ref={clickable}/>
+          <i className="fab fa-linkedin footer-brand-icon"
+              onClick = {(e) => { 
+                e.preventDefault();
+                window.location.href = 'https://www.linkedin.com/in/sevanetrebchenko/';
+            }}/>
         </button>
 
         <button className="footer-button">
-          <i className="fab fa-twitter footer-brand-icon" ref={clickable}/>
+          <i className="fab fa-twitter footer-brand-icon"
+              onClick = {(e) => { 
+              e.preventDefault();
+              window.location.href = 'https://twitter.com/netrebchenko/';
+          }}/>
         </button>
 
       </div>
