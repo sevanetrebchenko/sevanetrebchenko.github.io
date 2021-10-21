3340aedfe2e2134d04507d91c33c1ce3b70c8075
seva <seva.netrebchenko@gmail.com>
2021-10-21 08:16:56 -0700

    Resume link works.

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -3,10 +3,16 @@ import React from "react";
 import "./resume.css"
 
 export default function Resume(props = {}) {
+
+  const RouteToResume = (e) => {
+    e.preventDefault();
+    window.location.href = "https://drive.google.com/file/d/1kquGDhURf46yFYBPrqXLWGi622AQMdEj/view?usp=sharing";
+  };
+
   return (
       <div className="resume">
         <img src="/resume/SevaNetrebchenko_Resume.svg" alt="2021 Resume"/>
-        <button className="download">
+        <button className="download" onClick={RouteToResume}>
           Download
         </button>
       </div>
