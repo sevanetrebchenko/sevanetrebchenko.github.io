5bdb121ae2a62874e92644d86ca916c29ca148af
sevanetrebchenko <seva.netrebchenko@gmail.com>
2023-05-10 03:53:33 -0700

    Fixed images in showcase displaying the wrong translation on screen resize.

--- src/pages/projects/featured-project.js
+++ src/pages/projects/featured-project.js
@@ -1,10 +1,184 @@
 
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, createRef } from 'react';
 
 import { hasClassName, addClassName, removeClassName, toMilliseconds, disableScrolling, enableScrolling } from '../../util/util';
 
+function roundTo(number) {
+    return Math.round(number * 10) / 10;
+}
+
+// Returns a reference to the state so that it can be used in a stale context.
+function useStateRef(initialValue) {
+    const [state, _setState] = useState(initialValue);
+    const stateRef = useRef(state);
+
+    const setState = (value) => {
+        stateRef.current = value;
+        _setState(value);
+    };
+
+    return [stateRef, setState];
+}
+
+
+function ImageShowcase(props) {
+    const { images, showCaptions = false } = props;
+
+    const [currentImageIndexRef, setImageIndex] = useStateRef(0);
+    const [currentTranslationRef, setTranslation] = useStateRef(0);
+    const [imageCaptionRefs, setImageCaptionRefs] = useState([]); // Array of references to image captions
+
+    const imageContainerRef = useRef(null);
+    let imageContainerStyle = {
+        transform: `translateX(${currentTranslationRef.current}px)`,
+    };
+
+    const changeCaption = function (targetCaptionIndex) {
+        if (!showCaptions) {
+            return;
+        }
+
+        // Apply fade-out transition for all captions.
+        for (let imageCaptionRef of imageCaptionRefs) {
+            removeClassName(imageCaptionRef.current, 'active');
+        }
+
+        // Apply fade-in transition to desired caption.
+        setTimeout(() => {
+            addClassName(imageCaptionRefs[targetCaptionIndex].current, 'active');
+        }, toMilliseconds(0.4));
+    }
+
+    const toPreviousImage = function (e) {
+        e.preventDefault();
+
+        // Apply translation to image carousel
+        const imageWidth = imageContainerRef.current.getBoundingClientRect().width;
+        const previousImageIndex = Math.max(currentImageIndexRef.current - 1, 0);
+
+        const translation = currentTranslationRef.current + (currentImageIndexRef.current - previousImageIndex) * imageWidth;
+        setTranslation(translation);
+
+        addClassName(imageContainerRef.current, 'transition');
+
+        imageContainerStyle = {
+            transform: `translateX(${translation}px)`
+        };
+
+        // Apply transition animations to captions
+        changeCaption(currentImageIndexRef.current);
+        setImageIndex(previousImageIndex);
+
+        setTimeout(() => {
+            removeClassName(imageContainerRef.current, 'transition');
+        }, toMilliseconds(0.3))
+    }
+
+    const toNextImage = function (e) {
+        e.preventDefault();
+
+        // Apply translation to image carousel
+        const imageWidth = imageContainerRef.current.getBoundingClientRect().width;
+        const nextImageIndex = Math.min(currentImageIndexRef.current + 1, images.length - 1);
+
+        const translation = currentTranslationRef.current - (nextImageIndex - currentImageIndexRef.current) * imageWidth;
+        setTranslation(translation);
+
+        addClassName(imageContainerRef.current, 'transition');
+
+        imageContainerStyle = {
+            transform: `translateX(${translation}px)`
+        };
+
+        // Apply transition animations to captions
+        changeCaption(nextImageIndex);
+        setImageIndex(nextImageIndex);
+
+        setTimeout(() => {
+            removeClassName(imageContainerRef.current, 'transition');
+        }, toMilliseconds(0.3))
+    }
+
+    // Fill image caption ref array
+    useEffect(() => {
+        // Assuming number of images does not change
+        setImageCaptionRefs(() => (
+            Array(images.length).fill().map((_, i) => imageCaptionRefs[i] || createRef())
+        ));
+
+        function onWindowResize(e) {
+            console.log('resizing');
+            console.log(currentImageIndexRef.current);
+
+            // Resizing the screen invalidates the translation because the image container size changes
+            // Recalculate translation based on new image container size
+            const imageWidth = imageContainerRef.current.getBoundingClientRect().width;
+            const translation = 0 - currentImageIndexRef.current * imageWidth;
+            setTranslation(translation);
+
+            imageContainerStyle = {
+                transform: `translateX(${translation}px)`
+            };
+        }
+
+        window.addEventListener('resize', onWindowResize);
+
+        return () => {
+            // Unmounting
+            window.removeEventListener('resize', onWindowResize);
+        };
+    }, []);
+
+    return (
+        <div className='image-container-parent'>
+            <div className='image-container'>
+                <div className='image-showcase' ref={imageContainerRef} style={imageContainerStyle}>
+                    <div className='images'>
+                        {
+                            images.map((image, index) => (
+                                <img src={image.src} key={index}></img>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className='image-navigation'>
+                    <div className='arrows'>
+                        <div className='arrow' onClick={toPreviousImage}>
+                            <i className='fa-solid fa-angle-left fa-fw'></i>
+                        </div>
+                        <div className='arrow' onClick={toNextImage}>
+                            <i className='fa-solid fa-angle-right fa-fw'></i>
+                        </div>
+                    </div>
+                </div>
+                {
+                    showCaptions && <div className='image-captions'>
+                        {
+                            images.map((image, index) => {
+                                let classNames = ['caption'];
+                                if (index == 0) {
+                                    classNames.push('active');
+                                }
+
+                                return (
+                                    <div className={classNames.join(' ')} key={index} ref={imageCaptionRefs[index]}>
+                                        <span className='title'>{image.title}</span>
+                                        <span className='description'>{image.description}</span>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                }
+            </div>
+        </div>
+
+    );
+}
+
+
 function FeaturedProjectImageShowcase(props) {
-    const { images } = props;
+    const { images, showCaptions } = props;
 
     const [currentImageIndex, setImageIndex] = useState(0);
     const [currentTranslation, setTranslation] = useState(0);
@@ -113,26 +287,20 @@ function FeaturedProjectImageShowcase(props) {
                     }
                 </div>
             </div>
+
+            <div className='image-captions'>
+
+            </div>
         </React.Fragment>
     );
-}
-
-function FeaturedProjectOverlayImageShowcase(props) {
-    const { images } = props;
-
-    const [currentImageIndex, setImageIndex] = useState(0);
-    const [currentTranslation, setTranslation] = useState(0);
-
-    const imageContainer = useRef(null);
-    let imageContainerStyle = {
-        transform: `translateX(${currentTranslation}px)`,
-    };
 }
 
 export default function FeaturedProject(props) {
     const { project } = props;
 
     const [isOverlayVisible, setOverlayVisible] = useState(false);
+    const overlayContainerRef = useRef(null);
+    const overlayRef = useRef(null);
 
     const openOverlay = function () {
         const overlayContainer = overlayContainerRef.current;
@@ -140,8 +308,8 @@ export default function FeaturedProject(props) {
 
         removeClassName(overlayContainer, 'hidden');
         overlay.offsetHeight; // Force browser repaint.
-        addClassName(overlayContainer, 'featured-project-overlay-open');
-        addClassName(overlay, 'featured-project-overlay-open');
+        addClassName(overlayContainer, 'open');
+        addClassName(overlay, 'open');
 
         disableScrolling();
     }
@@ -150,8 +318,8 @@ export default function FeaturedProject(props) {
         const overlayContainer = overlayContainerRef.current;
         const overlay = overlayRef.current;
 
-        removeClassName(overlay, 'featured-project-overlay-open');
-        removeClassName(overlayContainer, 'featured-project-overlay-open');
+        removeClassName(overlay, 'open');
+        removeClassName(overlayContainer, 'open');
 
         setTimeout(() => {
             addClassName(overlayContainer, 'hidden');
@@ -160,135 +328,65 @@ export default function FeaturedProject(props) {
         enableScrolling();
     }
 
-    // useEffect(() => {
-    //     // Mounting.
-    //     const overlayContainer = overlayContainerRef.current;
-    //     const overlay = overlayRef.current;
+    useEffect(() => {
+        // Mounting.
+        const overlayContainer = overlayContainerRef.current;
+        const overlay = overlayRef.current;
 
-    //     function onClickOutside(e) {
-    //         // Do not attempt to close a closed overlay.
-    //         if (hasClassName(overlayContainer, 'hidden')) {
-    //             return;
-    //         }
+        function onClickOutside(e) {
+            // Do not attempt to close a closed overlay.
+            if (hasClassName(overlayContainer, 'hidden')) {
+                return;
+            }
 
-    //         if (!overlay.contains(e.target)) {
-    //             closeOverlay();
-    //         }
-    //     }
+            if (!overlay.contains(e.target)) {
+                closeOverlay();
+            }
+        }
 
-    //     addClassName(overlayContainer, 'hidden'); // Container holding overlay starts off as hidden.
-    //     document.addEventListener('mousedown', onClickOutside);
+        addClassName(overlayContainer, 'hidden'); // Container holding overlay starts off as hidden.
+        document.addEventListener('mousedown', onClickOutside);
 
-    //     return () => {
-    //         // Unmounting.
-    //         document.removeEventListener('mousedown', onClickOutside);
-    //         enableScrolling();
-    //     };
-    // }, []); // Run only when mounting / unmounting.
+        return () => {
+            // Unmounting.
+            document.removeEventListener('mousedown', onClickOutside);
+            enableScrolling();
+        };
+    }, []); // Run only when mounting / unmounting.
 
-    const overlayContainerRef = useRef(null);
-    const overlayRef = useRef(null);
 
     return (
         <div className='featured-project'>
-            <div className='content'>
-                <FeaturedProjectImageShowcase images={project.images}></FeaturedProjectImageShowcase>
-                <div className='banner'>
-                    <span className='title'>Featured Project</span>
-                    <div className='header'>
-                        <span className='title'>{project.title}</span>
-                        <div className='info'>
-                            <i className='fa-solid fa-question fa-fw' onClick={openOverlay}></i>
+            <ImageShowcase images={project.images} showCaptions={false}></ImageShowcase>
+            <div className='banner'>
+                <span className='title'>Featured Project</span>
+                <div className='header'>
+                    <span className='title'>{project.title}</span>
+                    <div className='info'>
+                        <i className='fa-solid fa-question fa-fw' onClick={openOverlay}></i>
+                    </div>
+                </div>
+            </div>
+            <div className='overlay-container' ref={overlayContainerRef}>
+                <div className='overlay' ref={overlayRef}>
+                    <ImageShowcase images={project.images} showCaptions={true}></ImageShowcase>
+                    <div className='content'>
+                        <i className='fa-solid fa-xmark fa-fw featured-project-overlay-close' onClick={closeOverlay}></i>
+
+                        <span className='featured-project-overlay-section-title'>Featured Project</span>
+                        <span className='featured-project-overlay-title'>{project.title}</span>
+                        <span className='featured-project-overlay-time'>July 2022 - Present (8 months)</span>
+
+                        <span className='featured-project-overlay-section-title'>About</span>
+                        <span className='featured-project-overlay-description'>{project.description}</span>
+
+                        <div className='cta'>
+                            <span>Read More</span>
+                            <i className='fa-solid fa-angle-right fa-fw'></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-
-
-        // <div className='featured-project'>
-        //     <div className='featured-project-container'>
-        //         <FeaturedProjectImageShowcase images={project.images}></FeaturedProjectImageShowcase>
-
-        //         <div className='featured-project-container-project-banner'>
-
-        //         </div>
-
-        //         <span className='featured-project-banner'>Featured Project</span>
-        //         <div className='featured-project-header'>
-        //             <span className='featured-project-title'>{project.title}</span>
-        //             <div className='featured-project-info'>
-        //                 <i className='fa-solid fa-question fa-fw' onClick={openOverlay}></i>
-        //             </div>
-        //         </div>
-        //     </div>
-
-        //     <div className='featured-project-overlay-container' ref={overlayContainerRef}>
-        //         <div className='featured-project-overlay' ref={overlayRef}>
-        //             {/* <div className='featured-project-overlay-image-showcase'>
-        //                 <div className='featured-project-images'>
-        //                     {
-        //                         project.images.map((image, index) => (
-        //                             <img src={image.src} key={index} className='featured-project-image'></img>
-        //                         ))
-        //                     }
-        //                 </div>
-
-        //                 <div className='featured-project-overlay-image-captions'>
-        //                     {
-        //                         project.images.map((image, index) => (
-        //                             <div className='featured-project-overlay-image-caption' key={index}>
-        //                                 <span className='featured-project-overlay-image-caption-title'>{image.title}</span>
-        //                                 <span className='featured-project-overlay-image-caption-description'>{image.description}</span>
-        //                             </div>
-        //                         ))
-        //                     }
-        //                 </div>
-
-        //                 <div className='featured-project-overlay-image-navigation'>
-        //                     <div className='featured-project-navigation-arrows'>
-        //                         <i className='fa-solid fa-angle-left fa-fw featured-project-navigation-previous'></i>
-        //                         <i className='fa-solid fa-angle-right fa-fw featured-project-navigation-next'></i>
-        //                     </div>
-        //                     <div className='featured-project-navigation-buttons'>
-        //                         {
-        //                             imageContainerNavigationButtons.map((element, index) => (
-        //                                 <React.Fragment key={index}>{element}</React.Fragment>
-        //                             ))
-        //                         }
-        //                     </div>
-        //                 </div>
-        //             </div>
-
-        //             <div className='featured-project-navigation'>
-        //                 <div className='featured-project-navigation-arrows'>
-        //                     <i className='fa-solid fa-angle-left fa-fw featured-project-navigation-previous' onClick={navigateToPreviousImage}></i>
-        //                     <i className='fa-solid fa-angle-right fa-fw featured-project-navigation-next' onClick={navigateToNextImage}></i>
-        //                 </div>
-        //                 <div className='featured-project-navigation-buttons'>
-        //                     {
-        //                         imageContainerNavigationButtons.map((element, index) => (
-        //                             <React.Fragment key={index}>{element}</React.Fragment>
-        //                         ))
-        //                     }
-        //                 </div>
-        //             </div> */}
-
-        //             <div className='featured-project-overlay-content'>
-        //                 <i className='fa-solid fa-xmark fa-fw featured-project-overlay-close' onClick={closeOverlay}></i>
-        //                 <span className='featured-project-overlay-section-title'>Featured Project</span>
-        //                 <span className='featured-project-overlay-title'>{project.title}</span>
-        //                 <span className='featured-project-overlay-time'>July 2022 - Present (8 months)</span>
-        //                 <span className='featured-project-overlay-section-title'>About</span>
-        //                 <span className='featured-project-overlay-description'>{project.description}</span>
-        //                 <div className='featured-project-overlay-cta'>
-        //                     <span className='featured-project-overlay-cta-text'>Read More</span>
-        //                     <i className='fa-solid fa-angle-right fa-fw featured-project-overlay-cta-icon'></i>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
