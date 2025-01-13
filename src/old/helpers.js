822b527f4c5c31794447e402c581dbe355758d29
sevanetrebchenko <seva.netrebchenko@gmail.com>
2025-01-13 00:22:22 -0800

    Removing old files. Updating blog post paths

--- src/old/helpers.js
+++
@@ -1,9 +0,0 @@
-
-function getPostUrl(title) {
-    // Remove all non-word characters and replace all spaces with -
-    return `/post/${title.toLowerCase().replace(/[^\w\s]/g, '').replace(/\s/g, '-')}`;
-}
-
-export {
-    getPostUrl,
-}
